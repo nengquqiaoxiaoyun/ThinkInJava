cf6c8048395cc9cdb71d3df59fe46c517cd2bcec
huakaimay <huakaimay@gmail.com>
2020-12-14 14:30:59 +0800

    通配符

---
+++ src/cn/cnhoyun/chapter15/whildcard/NonCovariantGenerics.java
@@ -0,0 +1,8 @@
+package cn.cnhoyun.chapter15.whildcard;// generics/NonCovariantGenerics.java
+// {WillNotCompile}
+
+
+public class NonCovariantGenerics {
+    // Compile Error: incompatible types:
+//    List<Fruit> flist = new ArrayList<Apple>();
+}
