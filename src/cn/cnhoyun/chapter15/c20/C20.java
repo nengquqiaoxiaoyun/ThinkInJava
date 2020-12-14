18a6ce02c641fdf3ad6776be38f349a0939d3be0
huakaimay <huakaimay@gmail.com>
2020-12-14 08:42:03 +0800

    练习20

---
+++ src/cn/cnhoyun/chapter15/c20/C20.java
@@ -0,0 +1,11 @@
+package cn.cnhoyun.chapter15.c20;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-12-14
+ */
+public interface C20 {
+    void a();
+
+    void b();
+}
---
+++ src/cn/cnhoyun/chapter15/c20/ImpC20.java
@@ -0,0 +1,28 @@
+package cn.cnhoyun.chapter15.c20;
+
+/**
+ * @author: huakaimay
+ * @since: 2020-12-14
+ */
+public class ImpC20 implements C20 {
+    @Override
+    public void a() {
+        System.out.println(this.getClass().getSimpleName() + " :a");
+    }
+
+    @Override
+    public void b() {
+        System.out.println(this.getClass().getSimpleName() + " :b");
+    }
+
+    <T extends C20> void c(T t) {
+        t.a();
+        t.b();
+    }
+
+    public static void main(String[] args) {
+        ImpC20 impC20 = new ImpC20();
+
+        impC20.c(new ImpC20());
+    }
+}
