6ce2696916a59ea164d4bf9fe2bbbef06ff903c2
huakaimay <huakaimay@gmail.com>
2020-12-15 11:14:33 +0800

    chapter15

---
+++ src/cn/cnhoyun/chapter15/ListOfInt.java
@@ -0,0 +1,18 @@
+package cn.cnhoyun.chapter15;// generics/ListOfInt.java
+// Autoboxing compensates for the inability
+// to use primitives in generics
+
+import java.util.*;
+import java.util.stream.*;
+
+public class ListOfInt {
+    public static void main(String[] args) {
+        List<Integer> li = IntStream.range(38, 48)
+                .boxed() // Converts ints to Integers
+                .collect(Collectors.toList());
+        System.out.println(li);
+    }
+}
+/* Output:
+[38, 39, 40, 41, 42, 43, 44, 45, 46, 47]
+*/
