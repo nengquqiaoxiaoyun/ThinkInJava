8d1b9170e65f6506e77d1525e8f29c1683b3262a
huakaimay <huakaimay@gmail.com>
2020-12-16 10:32:31 +0800

    多路分发

---
+++ src/cn/cnhoyun/chapter19/multiplexing/RoShamBo.java
@@ -0,0 +1,22 @@
+// enums/RoShamBo.java
+// (c)2020 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+// Common tools for RoShamBo examples
+package cn.cnhoyun.chapter19.multiplexing;
+
+
+import cn.cnhoyun.chapter19.Enums;
+
+public class RoShamBo {
+    public static <T extends Competitor<T>> void match(T a, T b) {
+        System.out.println(
+                a + " vs. " + b + ": " + a.compete(b));
+    }
+
+    public static <T extends Enum<T> & Competitor<T>>
+    void play(Class<T> rsbClass, int size) {
+        for (int i = 0; i < size; i++)
+            match(Enums.random(rsbClass), Enums.random(rsbClass));
+    }
+}
